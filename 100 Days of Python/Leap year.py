1e14ea534c43968ce505cdaa712d55af56a72817
Rahul-Tamilselvan <139439835+Rahul-Tamilselvan@users.noreply.github.com>
2023-08-02 12:55:21 +0530

    Update Leap year.py

--- 100 Days of Python/Leap year.py
+++ 100 Days of Python/Leap year.py
@@ -11,10 +11,10 @@ year = int(input("Enter the year : "))
 if year % 4 == 0:
   if year % 100 == 0:
     if year % 400 == 0:
-      print(f"The year {year}, is a leap year")
+      print("leap year")
     else:
-      print(f"The year {year}, is not a leap year")
+      print("not leap year")
   else:
-    print(f"The year {year}, is a leap year")
+    print("leap year")
 else:
-  print(f"The year {year}, is not a leap year")
+  print("not leap year")
