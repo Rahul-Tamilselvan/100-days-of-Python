4f5e1b6339b56788356ca87618bfbcbc7f16d116
Rahul-Tamilselvan <rahultamizhselvan@gmail.com>
2023-10-03 21:24:36 +0530

    11th Project

---
+++ 100 Days of Python/Hangman_words.py
@@ -0,0 +1,5 @@
+word_list = ["Banana", "Sunshine", "Galaxy", "Whisper", "Enchanted", "Radiant", "Adventure",
+             "Mirage", "Harmony", "Velvet", "Serendipity", "Glisten", "Quixotic", "Tranquil",
+             "Labyrinth", "Umbrella", "Cascade", "Jigsaw", "Paradox", "Ephemeral", "Sizzle",
+             "Pinnacle", "Wanderlust", "Blossom", "Intrigue", "Serenity", "Quench", "Luminary",
+             "Mosaic", "Mellifluous"]
